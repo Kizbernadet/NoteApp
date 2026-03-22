37e6be90ec78bf7b223e03486ae1f4f76bf5d7c2
Kizbernadet <kizbernadet4@GMAIL.com>
2026-03-22 16:06:13 +0200

    ajout des modèles - debut

---
+++ backend/models/user.js
@@ -0,0 +1,38 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js"; // Ajout du .js
+
+const User = sequelize.define('User', {
+  // Pas besoin de déclarer ID si tu suis la convention, 
+  // mais c'est mieux de le mettre pour être explicite :
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  first_name: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  last_name: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  }, 
+  account_id: {
+    type: DataTypes.INTEGER, 
+    allowNull: false, 
+    references: {
+        model: 'accounts', 
+        key: 'id'
+    }
+  }
+}, {
+  tableName: 'users',
+  timestamps: true,
+  // Si tes colonnes SQL s'appellent exactement created_at (avec underscore)
+  // on ajoute ces deux lignes :
+  createdAt: 'created_at',
+  updatedAt: 'updated_at', 
+  underscored: true
+});
+
+export default User;
